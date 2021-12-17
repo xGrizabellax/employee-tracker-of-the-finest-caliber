3b06a1f1955f49076011f283201244b8714acdb0
Sam Page <sampage6@gmail.com>
2021-12-17 03:14:30 -0600

    cleaned up funcionality

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const inquirer = require('inquirer')
 const selectStr = require('./helpers/utils')
-const employees = []
 const mysql = require('mysql2');
-const { initial } = require('lodash');
-// const dbDepAdd = require('./helpers/utils');
+const consoleTable = require('console.table');
+const { forEach } = require('lodash');
 
 const db = mysql.createConnection(
     {
@@ -30,6 +29,7 @@ const rootQue = [{
         "Quit"
     ]
 }]
+
 const departmentQue = [{
     type: "input",
     name: "depName",
@@ -45,55 +45,55 @@ const departmentQue = [{
 const roleQue = [{
     type: "input",
     name: "roleTitle",
-    // when: (answers) => answers.track === "Add a role",
     message: "Please enter the role title"
 },
 {
     type: "input",
     name: "roleSal",
-    // when: (answers) => answers.track === "Add a role",
     message: "Please enter the salary of the role"
 },
 {
     type: "input",
     name: "roleDep",
-    // when: (answers) => answers.track === "Add a role",
     message: "Please enter the department of the role"
 }];
 
 const employeeQue = [{
     type: "input",
     name: "empFirst",
-    // when: (answers) => answers.track === "Add an employee",
     message: "Please enter the employee's first name"
 },
 {
     type: "input",
     name: "empLast",
-    // when: (answers) => answers.track === "Add an employee",
     message: "Please enter the employee's last name"
 },
 {
     type: "input",
     name: "empRole",
-    // when: (answers) => answers.track === "Add an employee",
-    message: "Please enter the employee's role-id"
+    message: "Please enter the employee's role-id",
+    validate: (data) => {
+        if (isNaN(data)) {
+            return `You did not enter a valid number`;
+        } else if (data === '') {
+            return `Please enter your role-id`
+        }
+        return true;
+    }
 },
 {
     type: "input",
     name: "empMang",
-    // when: (answers) => answers.track === "Add an employee",
     message: "Please enter the manager-id the employee will be under"
 }]
 
-const updateEmployee = [{
-    type: "list",
-    name: "empSelect",
-    when: (answers) => answers.track === "Update an employee role",
-    message: "Which employee would you like to update?",
-    choices: employees
-},
-];
+// const updateEmployee = [{
+//     type: "list",
+//     name: "empSelect",
+//     message: "Which employee would you like to update?",
+//     choices: employees
+// },
+// ];
 
 
 
@@ -105,29 +105,27 @@ const rootQuestion = async () => {
         case 'View all roles':
         case 'View all employees':
             viewAll(choice);
-            // rootQuestion();
+            rootQuestion();
             break;
         case 'Add a department':
             generateDepartment();
-            // rootQuestion();
             break;
         case 'Add a role':
             generateRole();
-            // rootQuestion();
             break;
         case 'Add an employee':
             generateEmployee();
-            // rootQuestion();
             break;
-        
-    }
-    rootQuestion();
+        case 'Update an employee role':
+            updateEmployee();
+            break;
 
+    }
 }
 
 function viewAll(choice) {
-const choiceArray = choice.split(' ');
-const table = choiceArray[2].slice(0,-1)
+    const choiceArray = choice.split(' ');
+    const table = choiceArray[2].slice(0, -1)
 
     db.query(selectStr, table, function (err, results) {
         console.table(results);
@@ -136,35 +134,59 @@ const table = choiceArray[2].slice(0,-1)
 
 const generateDepartment = async () => {
     const depAnswers = await inquirer.prompt(departmentQue);
-    
     db.query(`INSERT INTO department (name) VALUES ("${depAnswers.depName}")`, function (err, results) {
         console.table(results);
     });
+    rootQuestion();
 }
 
 const generateRole = async () => {
     const roleAnswers = await inquirer.prompt(roleQue)
-    queryRole(roleAnswers)
-}
-function queryRole(roleAnswers) {
     db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${roleAnswers.roleTitle}", "${roleAnswers.roleSal}", "${roleAnswers.roleDep}")`, function (err, results) {
         console.table(results);
     });
+    rootQuestion();
 }
 
 const generateEmployee = async () => {
     const empAnswers = await inquirer.prompt(employeeQue)
-    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answers.empFirst}", "${answers.empLast}", "${answers.empRole}", "${answers.empMang}")`, function (err, results) {
+    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${empAnswers.empFirst}", "${empAnswers.empLast}", "${empAnswers.empRole}", "${empAnswers.empMang}")`, function (err, results) {
         console.table(results);
     });
     const employee = `${empAnswers.empFirst} ${empAnswers.empLast}`
-    employees = employees.push(employee)
+    employees.push(employee)
+    rootQuestion();
+}
+
+const updateEmployee = async () => {
+    // const employees = db.query(`SELECT * FROM employee`);
+    db.query(`SELECT * FROM employee`, function (err, res) {
+        if (err) throw err;
+        // console.log(res)
+        const empArray = []
+        res.forEach(res => empArray.push(res.first_name))
+        console.log(empArray)
+        const updateAnswers = inquirer.prompt()
+    });
+
 }
 
 function init() {
-rootQuestion()
+    rootQuestion()
 
 }
+
+db.query(`SELECT * FROM employee`, function (err, res) {
+    if (err) throw err;
+    // console.log(res)
+    const empArray = []
+    res.forEach(res => empArray.push(res.first_name))
+    console.log(empArray)
+});
+
+
+
+
 
 init()
 // module.exports = { viewAllDepartments, viewAllRoles, viewAllEmployees, generateDepartment, generateRole, generateEmployee, switchOperator, rootQuestion }
@@ -181,7 +203,7 @@ init()
 
 // const trackTeam = async () => {
 //     const answers = await inquirer.prompt(genQue);
-    
+
 // }
 
 // const trackEmployees = async () => {
